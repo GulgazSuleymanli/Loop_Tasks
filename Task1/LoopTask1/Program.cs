bbb693347c78ed2e775a9b8359e32a23989db9a0
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2023-09-27 19:19:29 +0400

    Completed Loop_Tasks

---
+++ Task1/LoopTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LoopTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 62;
+            int i = 1;
+            while (i < num)
+            {
+                i *= 2;
+            }
+            if (i == num)
+            {
+                Console.WriteLine("Eded ikinin quvvetidir");
+            }
+            else
+            {
+                Console.WriteLine("Eded ikinin quvveti deyil");
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5724;
+            int a = 0;
+            while (num>=1)
+            {
+                a = a * 10 + (num % 10);
+                num = (num - num % 10) / 10;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
